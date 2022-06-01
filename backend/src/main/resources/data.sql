624172176191057b521a5d30ab3e3ed6d853dd94
luanalimadeveloper <luanalima.developer@gmail.com>
2022-05-31 22:51:05 -0300

    Enrollment seed

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -24,4 +24,7 @@ INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) 
 
 INSERT INTO tb_section(title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 1', 'Neste capítulo vamos começar', 1, 'https://www.stockvault.net/data/2018/07/03/252865/preview16.jpg', 1, null);
 INSERT INTO tb_section(title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 1', 'Neste capítulo vamos continuar', 2, 'https://www.stockvault.net/data/2018/07/03/252865/preview16.jpg', 1, 1);
-INSERT INTO tb_section(title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 1', 'Neste capítulo vamos finalizar', 3, 'https://www.stockvault.net/data/2018/07/03/252865/preview16.jpg', 1, 2)
+INSERT INTO tb_section(title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 1', 'Neste capítulo vamos finalizar', 3, 'https://www.stockvault.net/data/2018/07/03/252865/preview16.jpg', 1, 2);
+
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2020-11-20T13:00:00.12345Z', null, true, false);
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2020-11-20T13:00:00.12345Z', null, true, false);
